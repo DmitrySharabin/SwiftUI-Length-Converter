a7ccbd22dcd4c329e7f5f75641b40ea0a62d43e9
Dmitry Sharabin <dmitrysharabin@gmail.com>
2021-10-22 15:32:02 +0300

    Initial Commit

---
+++ Length Converter/Length_ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Length_ConverterApp.swift
+//  Length Converter
+//
+//  Created by Dmitry Sharabin on 22.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct Length_ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
