e83cbcb1f4f83376a363cc19b0432834f65ccf99
Dmitry Sharabin <dmitrysharabin@gmail.com>
2021-10-22 17:10:33 +0300

    Day 19 — Challenge day

--- Length Converter/ContentView.swift
+++ Length Converter/ContentView.swift
@@ -8,9 +8,103 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var input = 0.0
+    @State private var selectedInputUnit = "in"
+    @State private var selectedOutputUnit = "m"
+    @FocusState private var inputIsFocused: Bool
+    
+    private let units = ["in", "ft", "yd", "m", "km", "mi"]
+    
+    var output: Double {
+        let inputUnit: UnitLength
+        let outputUnit: UnitLength
+        
+        switch selectedInputUnit {
+            case "in":
+                inputUnit = .inches
+            case "ft":
+                inputUnit = .feet
+            case "yd":
+                inputUnit = .yards
+            case "m":
+                inputUnit = .meters
+            case "km":
+                inputUnit = .kilometers
+            case "mi":
+                inputUnit = .miles
+            default:
+                inputUnit = .inches
+        }
+        
+        switch selectedOutputUnit {
+            case "in":
+                outputUnit = .inches
+            case "ft":
+                outputUnit = .feet
+            case "yd":
+                outputUnit = .yards
+            case "m":
+                outputUnit = .meters
+            case "km":
+                outputUnit = .kilometers
+            case "mi":
+                outputUnit = .miles
+            default:
+                outputUnit = .meters
+        }
+        
+        let inputMeasurement = Measurement(value: input, unit: inputUnit)
+        let outputMeasurement = inputMeasurement.converted(to: outputUnit)
+        
+        return outputMeasurement.value
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Enter a number", value: $input, format: .number)
+                        .keyboardType(.decimalPad)
+                        .focused($inputIsFocused)
+                    
+                    Picker("Choose unit", selection: $selectedInputUnit) {
+                        ForEach(units, id: \.self) {
+                            Text($0)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                } header: {
+                    Text("Convert")
+                }
+                
+                Section {
+                    Picker("Choose unit", selection: $selectedOutputUnit) {
+                        ForEach(units, id: \.self) {
+                            Text($0)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                } header: {
+                    Text("Into")
+                }
+                
+                Section {
+                    Text(output.formatted())
+                } header: {
+                    Text("Result")
+                }
+            }
+            .navigationTitle("Length Converter")
+            .toolbar {
+                ToolbarItemGroup(placement: .keyboard) {
+                    Spacer()
+                    
+                    Button("Done") {
+                        inputIsFocused = false
+                    }
+                }
+            }
+        }
     }
 }
 
